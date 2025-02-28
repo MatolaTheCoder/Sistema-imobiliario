2dea94cdddb3345150ce6991ecf1edb79d7cffd9
MatolaTheCoder <isacmatolajr6@gmail.com>
2025-02-28 15:11:09 +0200

    Primeiro comit laravel, vue e inertia configurados com sucesso

---
+++ JayImobiliaria/resources/js/Pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h1 class="text-3xl bg-amber-300">Inicio {{ counter }}</h1>
+    </div>
+  </template>
+
+  <script setup>
+  import { ref, onMounted } from "vue";
+
+  const counter = ref(0);
+
+  onMounted(() => {
+    setInterval(() => {
+      counter.value++;
+    }, 1000); // Atualiza a cada 1 segundo
+  });
+  </script>
+
+  <style scoped>
+  </style>
