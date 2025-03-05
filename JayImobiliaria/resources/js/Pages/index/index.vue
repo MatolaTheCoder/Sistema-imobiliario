f31c93131d889e4a6fc6a9ef420c0e629d768cf7
MatolaTheCoder <isacmatolajr6@gmail.com>
2025-03-05 16:19:14 +0200

    feat: importacao do css

--- JayImobiliaria/resources/js/Pages/index/index.vue
+++ JayImobiliaria/resources/js/Pages/index/index.vue
@@ -1,15 +1,19 @@
-<template>
+<template >
   <div>
     <MainLayout>
-      <h1 class="text-3xl bg-amber-300">Inicio {{ nome }}</h1>
+      <h1 class="text-7xl bg-amber-300">Inicio {{ nome }}</h1>
     </MainLayout>
   </div>
 </template>
 
 <script setup>
 import { defineProps } from 'vue';
+import '../../../css/app.css';
 import MainLayout from '../../Layouts/MainLayout.vue';
 defineProps({ nome: String });
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
+
