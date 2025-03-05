a55a0c01c19afae949d9fe53a79bd5ab957708fd
MatolaTheCoder <isacmatolajr6@gmail.com>
2025-03-05 13:42:14 +0200

    feat: preparar os modelos

---
+++ JayImobiliaria/app/Models/Casa.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Casa extends Model
+{
+    protected $fillable = [
+        'endereco',
+        'cidade',
+        'bairro',
+        'preco_venda',
+        'preco_aluguel',
+        'complemento',
+        'largura',
+        'descricao',
+        'quartos',
+        'banheiros',
+        'mobiliado',
+        'estado_casa_id',
+        'descricao_mobilia',
+    ];
+
+    public function estadoCasa()
+    {
+        return $this->belongsTo(EstadoCasa::class);
+    }
+}
