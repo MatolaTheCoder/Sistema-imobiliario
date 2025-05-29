866533b072f1a5e27e368de4023cc1996f4a51fd
MatolaTheCoder <isacmatolajr6@gmail.com>
2025-05-29 13:50:22 +0200

    testando o envio de dados para o template

--- JayImobiliaria/resources/js/Pages/index/index.vue
+++ JayImobiliaria/resources/js/Pages/index/index.vue
@@ -1,18 +1,20 @@
-<template >
-  <div>
-    <MainLayout>
-      <h1 class="text-2xl bg-amber-600">Inicio {{ nome }}</h1>
-    </MainLayout>
-  </div>
+<template>
+    <div>
+        <MainLayout>
+            <h1 class="text-2xl bg-amber-600">Inicio {{ nome }}</h1>
+            <div v-for="casa in casas" :key="casa.id">
+                <p>{{ casa.nome }}</p>
+                <p>{{ casa.cidade }}</p>
+                <p>{{ casa.valor }}</p>
+            </div>
+        </MainLayout>
+    </div>
 </template>
 
 <script setup>
 import { defineProps } from 'vue';
 import MainLayout from '../../Layouts/MainLayout.vue';
-defineProps({ nome: String });
+defineProps({ casas: Array });
 </script>
 
-<style scoped>
-
-</style>
-
+<style scoped></style>
