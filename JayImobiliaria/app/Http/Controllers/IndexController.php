a9248b9ef7eff690a03dbec3470aa2bde4608409
MatolaTheCoder <isacmatolajr6@gmail.com>
2025-05-29 13:51:52 +0200

    testando o envio de dados para o template

--- JayImobiliaria/app/Http/Controllers/IndexController.php
+++ JayImobiliaria/app/Http/Controllers/IndexController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\CasaService;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
+    protected $casaService;
+    public function __construct(CasaService $casaService) {
+        $this->casaService = $casaService;
+    }
     public function index() {
+        $casas = $this->casaService->getAll();
         return inertia(
             'index/index',
             [
-                'nome' => 'Isac',
+                'casas' => $casas,
             ]);
     }
 
