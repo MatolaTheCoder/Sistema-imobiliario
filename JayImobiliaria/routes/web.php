5ed919a3f081cc859b684acfeb502c0bab485240
MatolaTheCoder <isacmatolajr6@gmail.com>
2025-05-29 13:51:07 +0200

    feat: controlador da casa

--- JayImobiliaria/routes/web.php
+++ JayImobiliaria/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CasaController;
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/show', [IndexController::class, 'show']);
+Route::get('/casa',[ CasaController::class, 'creates']);
